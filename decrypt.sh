c9c139e03a2e410403072f4860f711505f19a391
Ian Bell <ian.h.bell@gmail.com>
2025-04-06 16:37:51 -0400

    Another try with script

--- decrypt.sh
+++ decrypt.sh
@@ -2,12 +2,13 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$REFPROP_GPG_PASSPHRASE" --output REFPROP.zip REFPROP.zip.gpg
-unzip REFPROP.zip -d REFPROP 
+gpg --quiet --batch --yes --decrypt --passphrase="$REFPROP_GPG_PASSPHRASE" --output REFPROPungpg.zip REFPROP.zip.gpg
+unzip REFPROPungpg.zip -d REFPROPungpg
+cd REFPROPungpg
 
 pip3 install numpy 
 
 git clone --recursive https://github.com/usnistgov/REFPROP-cmake
 cd REFPROP-cmake
-cmake -B bld -S . -DCMAKE_BUILD_TYPE=Release -DREFPROP_FORTRAN_PATH=`pwd`/../REFPROP/FORTRAN
+cmake -B bld -S . -DCMAKE_BUILD_TYPE=Release -DREFPROP_FORTRAN_PATH=`pwd`/../FORTRAN
 cmake --build bld --config Release 
